ee1a0168d839286322f1a1d43457b5dae1276bdc
nazirumar <nazirumar888@gmail.com>
2024-10-27 16:58:12 +0100

    password resetting done

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth import get_user_model
 import logging
 from .utils import account_activation_token
@@ -51,3 +52,34 @@ def send_activation_email_task(user_id):
         send_activation_email(user)
     except User.DoesNotExist:
         logger.error(f"User with id {user_id} does not exist")
+
+
+
+@shared_task
+def send_password_reset_email(user_id, domain, protocol='http'):
+    """
+    Task to send a password reset email to the user with user_id.
+    """
+    try:
+        user = User.objects.get(pk=user_id)
+        uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+        token = default_token_generator.make_token(user)
+        
+        reset_link = f"{protocol}://{domain}{reverse('password_reset_confirm', kwargs={'uidb64': uidb64, 'token': token})}"
+        
+        subject = "Password Reset Request"
+        message = render_to_string('accounts/password_reset_email.html', {
+            'user': user,
+            'reset_link': reset_link,
+        })
+        
+        send_mail(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            [user.email],
+            fail_silently=False,
+            html_message=message,
+        )
+    except User.DoesNotExist:
+        pass
--- accounts/urls.py
+++ accounts/urls.py
@@ -7,12 +7,10 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('register', views.register_view, name='register'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('password_change/', views.change_password, name='password_change'),
-    # path('password_change/done/', views.CustomPasswordChangeDoneView.as_view(), name='password_change_done'),
-
+    path('password_reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
+  
     path('activate/<uidb64>/<token>/', views.activate_account, name='activate'),
-    
-    # path('request-reset-email/',views.RequestResetEmailView.as_view(),name='request-reset-email'),
-    # path('set-new-password/<uidb64>/<token>',views.SetNewPasswordView.as_view(),name='set-new-password'),
-
 ]
